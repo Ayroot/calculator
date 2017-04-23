3ba29e1cfd104e1e577dbbeb1019dbd1a32809e8
Ayroot <ata24279@gmail.com>
2017-04-23 12:09:56 +0500

    1. Создан класс DataAdapter для GridView 2. Добавлен элемент GridView 3. Изменен файл MainActivity

---
+++ Calculator/app/src/main/java/com/ayroot/calculator/DataAdapter.java
@@ -0,0 +1,40 @@
+package com.ayroot.calculator;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by airat on 23.04.2017.
+ */
+
+public class DataAdapter extends ArrayAdapter<String> {
+
+    private static final String[] number ={
+            "1","2","3","4","5","6","7","8","9","0"
+    };
+
+    Context mContext;
+
+    public DataAdapter(Context context, int textViewResourceId) {
+        super(context, textViewResourceId, number);
+        this.mContext = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        TextView label = (TextView) convertView;
+        if (convertView==null) {
+            convertView = new TextView(mContext);
+            label = (TextView) convertView;
+        }
+        label.setText(number[position]);
+        return (convertView);
+    }
+    public String getItem(int position) {
+        return number[position];
+    }
+
+}
--- Calculator/app/src/main/java/com/ayroot/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/ayroot/calculator/MainActivity.java
@@ -1,13 +1,44 @@
 package com.ayroot.calculator;
 
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.TextView;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private TextView mSelectText;
+    private DataAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSelectText = (TextView) findViewById(R.id.info);
+        final GridView g = (GridView) findViewById(R.id.gridView);
+        mAdapter = new DataAdapter(getApplicationContext(),
+                android.R.layout.simple_list_item_1);
+        g.setAdapter(mAdapter);
+        g.setOnItemSelectedListener(this);
+        g.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mSelectText.setText(mAdapter.getItem(position));
+            }
+        });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+        mSelectText.setText(mAdapter.getItem(position));
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        mSelectText.setText("ничего");
     }
 }
